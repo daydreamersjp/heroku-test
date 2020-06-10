33a242f7ca99acbf861ab4947ce1b590d323a5e1
daydreamersjp <daydreamersjp@yahoo.co.jp>
2020-06-10 19:34:07 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,2 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World! updated."
+    
 if __name__=="__main__":
-    print("Hello World from app2.py")
+    app.run()
