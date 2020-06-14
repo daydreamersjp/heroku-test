1f1042a3132ad73177ed187d127bcd876afe6896
daydreamersjp <daydreamersjp@yahoo.co.jp>
2020-06-14 22:56:49 +0200

    update

--- make_data.py
+++ make_data.py
@@ -4,6 +4,6 @@ now = datetime.datetime.now()
 
 filename = str(now.year) + str(now.month) + str(now.day) + str(now.hour) + str(now.minute)
 
-f = open(filename, "w+")
+f = open('./data/'+filename, "w+")
 f.close()
 
